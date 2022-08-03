058a55ae90a3cdde0fae55837c5566a162e44ab1
justin reid <hi@justinre.id>
2022-08-03 10:58:38 +0100

    start of lab

---
+++ tests/pub_test.py
@@ -0,0 +1,13 @@
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
+    
+    def test_increase_till(self):
+        self.pub.increase_till(2.5)
+        self.assertEqual(102.5, self.pub.till)
